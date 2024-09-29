d8432a1e7d240136c249121a03017e36452dd9a1
Kadu <yogesh.kadu@capgemini.com>
2024-09-29 17:44:43 +0530

    corrected browser route import

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { BrowserRouter } from "react-router-dom";
 
 // App entrypoint
 function App() {
