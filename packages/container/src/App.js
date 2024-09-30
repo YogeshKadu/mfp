66f64efc839a368d6dfb2d801f7ce57e2a2ce1d0
Kadu <yogesh.kadu@capgemini.com>
2024-09-30 15:01:20 +0530

    Randomise production class name

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const GeneratedClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 // App entrypoint
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={GeneratedClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 }
